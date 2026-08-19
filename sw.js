bd7178212962748e6e128b490f72b71fdc6c6fc4
shu39a-lang <shu39a@gmail.com>
2026-08-19 22:47:16 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE="tenx-stock-v1";
+const SHELL=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting();});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener("fetch",e=>{
+  const r=e.request,u=new URL(r.url);
+  if(r.method!=="GET"||u.origin!==location.origin)return;
+  if(u.pathname.endsWith(".json")){
+    e.respondWith(fetch(r).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(r,copy));return res;}).catch(()=>caches.match(r)));
+    return;
+  }
+  if(r.mode==="navigate"){
+    e.respondWith(fetch(r).catch(()=>caches.match("./index.html")));
+    return;
+  }
+  e.respondWith(caches.match(r).then(hit=>hit||fetch(r).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(r,copy));return res;})));
+});
